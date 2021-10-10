5d8d3d057d1ca22dd3b73b9233c8387555c1f8ff
dabagirevalens <83093092+dabagirevalens@users.noreply.github.com>
2021-10-10 21:03:16 +0200

    working with projects

---
+++ projects/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import projects, project, createProject, updateProject, deleteProject
+
+urlpatterns = [
+    path('', projects, name='projects'),
+    path('project/<str:pk>', project, name='project'),
+
+    path('create-project', createProject, name='createproject'),
+    path('update-project/<str:pk>', updateProject, name='updateproject'),
+    path('delete-project/<str:pk>', deleteProject, name='deleteproject'),
+]
