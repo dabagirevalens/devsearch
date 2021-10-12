8bfb8b61d8a21da64bfc1daaf0673aee36ae3905
dabagirevalens <83093092+dabagirevalens@users.noreply.github.com>
2021-10-12 22:06:48 +0200

    projects review

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Project
+from .models import Project, Review
 from django import forms
 
 
@@ -24,3 +24,20 @@ class ProjectForm(ModelForm):
         #         'class': 'input input--text',
         #         'placeholder': 'Add a title'
         #         })
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+        labels = {
+            'value': 'Place you vote',
+            'body': 'Leave a comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {'class': 'input input--text' })
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 from .models import Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from .utils import paginateProjects, searchProjects
 
 
@@ -19,7 +20,23 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, 'single-project.html', {'project': projectObj})
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'single-project.html', {'project': projectObj, 'form' : form})
 
 
 @login_required(login_url='login')
