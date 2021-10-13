6de4c550f24111b5235ede2d6a0153e2847da0c8
dabagirevalens <83093092+dabagirevalens@users.noreply.github.com>
2021-10-13 16:00:12 +0200

    password reset

--- users/signals.py
+++ users/signals.py
@@ -1,8 +1,11 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 from django.contrib.auth.models import User
 from .models import Profile
+
 
 
 
@@ -16,6 +19,20 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+        subject = 'Wellcome to Devsearch'
+        message = 'We are grad you are here!'
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
+        )
+
+
+
 
 
 def updateProfile(sender, instance, created, **kwargs):
