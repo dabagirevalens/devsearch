2a288497f3737ae15e863f8e91df5544fbce2925
dabagirevalens <83093092+dabagirevalens@users.noreply.github.com>
2021-11-12 15:50:50 +0200

    static files fixed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -210,7 +210,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
